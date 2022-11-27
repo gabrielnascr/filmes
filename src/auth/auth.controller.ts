dac1b1460e09b3a29be7718f79b4e4b767d87fd6
gabrielnascr <gr153tt@gmail.com>
2022-11-27 19:18:39 -0300

    feat: adding profile route to get logged user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Get,
   Injectable,
   Post,
   Req,
@@ -9,15 +10,25 @@ import {
 import { AuthService } from './auth.service';
 import { AuthenticateDTO } from './dto/authenticate-dto';
 import { AuthGuard } from '@nestjs/passport';
+import { AdminService } from 'src/admin/admin.service';
 
 @Injectable()
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly adminService: AdminService,
+  ) {}
 
   @UseGuards(AuthGuard('local'))
   @Post('login')
   async authenticate(@Body() authenticateDTO: AuthenticateDTO) {
     return this.authService.authenticacte(authenticateDTO);
+  }
+
+  @Get('profile')
+  @UseGuards(AuthGuard('jwt'))
+  async profile(@Req() request) {
+    return this.adminService.findOneById(request.user.sub);
   }
 }
